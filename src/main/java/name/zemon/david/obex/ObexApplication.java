f9463c45ce75c8fcfaacc2d1a8e90e54ee845c3b
David Zemon <david@zemon.name>
2020-08-02 11:06:23 -0500

    Initial generation from spring.io

---
+++ src/main/java/name/zemon/david/obex/ObexApplication.java
@@ -0,0 +1,13 @@
+package name.zemon.david.obex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObexApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ObexApplication.class, args);
+  }
+
+}
