104009891c42d6f7750ffdfa95dbe43601090fc3
David Zemon <david@zemon.name>
2020-08-05 15:54:24 -0500

    Cleanup boilerplate code

--- src/main/java/name/zemon/david/obex/ObexApplication.java
+++ src/main/java/name/zemon/david/obex/ObexApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ObexApplication {
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     SpringApplication.run(ObexApplication.class, args);
   }
 
